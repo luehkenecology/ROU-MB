c14c08140d423fa4a8fbf784589732e3fbca38aa
luehkenecology <luehken65@gmail.com>
2018-01-18 09:18:01 +0100

    date information as posixct

--- 01-read.R
+++ 01-read.R
@@ -46,10 +46,12 @@ library(tidyr)
 ## year 2014------------------------------------------------------------------------------------------
 # read data 2014
 data2014 <- read.xlsx("data/bloodmeals-2014.xlsx", 1)
+
+# remove NAs
 data2014_2 <- data2014[1:2891,]
 
-# built new data.frame with relevant information
-data2014b <- data.frame(sample_id = as.character(data2014_2$Zusatz.bezeichnung),
+# built new data.frame only with relevant information
+data2014_3 <- data.frame(sample_id = as.character(data2014_2$Zusatz.bezeichnung),
                         site = as.character(fillNAgaps(data2014_2$Trapping.site)),
                         date = as.character(fillNAgaps(data2014_2$Date.of.collection)),
                         mosquito_species = as.character(data2014_2$Mosquitoe.species),
@@ -59,117 +61,118 @@ data2014b <- data.frame(sample_id = as.character(data2014_2$Zusatz.bezeichnung),
 adapt_2014 <- read.xlsx("data/adapt_2015.xlsx", 2)[1:57,]
 
 # identify samples by sample id and replace host species names 
-data2014b$host[c(match(adapt_2014$Zusatz.bezeichnung,data2014b$sample_id))] <- as.character(adapt_2014$Answer.Alex)
+data2014_3$host[c(match(adapt_2014$Zusatz.bezeichnung,data2014_3$sample_id))] <- as.character(adapt_2014$Answer.Alex)
 
 # homogenize names of host species
-data2014b$species_revalue <- revalue(data2014b$host, c("Anas sp." = "Anas spp.",      
-                                                       "Anopheles sp mit Kocher" = "mosquito DNA",              
-                                                       "Anopheles sp." = "mosquito DNA",                         
-                                                       "Anopheles sp. Mit Kocher" = "mosquito DNA",    
-                                                       "Bos taurus " = "Bos taurus",                          
-                                                       "Canis lupus familiaris" = "Canis lupus",            
-                                                       "Canis lupus familiaris famiaris" = "Canis lupus", 
-                                                       "Equus caballus " = "Equus caballus",                        
-                                                       "felis catus" = "Felis catus",                          
-                                                       "Hirundo rustica transitiva" = "Hirundo rustica",            
-                                                       "Homo sapiens " = "Homo sapiens",                        
-                                                       "Homo sapiens m. Kocher" = "Homo sapiens",
-                                                       "Homo sapiens sapiens"= "Homo sapiens",
-                                                       "kein ergebnis" = "negative",                   
-                                                       "Lepus europaeus " = "Lepus europaeus",                       
-                                                       "negativ" = "negative",
-                                                       "Rhinolophus h. hipposideros" = "Rhinolophus hipposideros",           
-                                                       "schlechte ident." = "negative",                     
-                                                       "sus scrofa" = "Sus scrofa",                         
-                                                       "unauswertbar" = "negative"))          
-
+data2014_3$host <- revalue(data2014_3$host, c("Anas sp." = "Anas spp.",      
+                                              "Anopheles sp mit Kocher" = "mosquito DNA",              
+                                              "Anopheles sp." = "mosquito DNA",                         
+                                              "Anopheles sp. Mit Kocher" = "mosquito DNA",    
+                                              "Bos taurus " = "Bos taurus",                          
+                                              "Canis lupus familiaris" = "Canis lupus",            
+                                              "Canis lupus familiaris famiaris" = "Canis lupus", 
+                                              "Equus caballus " = "Equus caballus",                        
+                                              "felis catus" = "Felis catus",                          
+                                              "Hirundo rustica transitiva" = "Hirundo rustica",            
+                                              "Homo sapiens " = "Homo sapiens",                        
+                                              "Homo sapiens m. Kocher" = "Homo sapiens",
+                                              "Homo sapiens sapiens"= "Homo sapiens",
+                                              "kein ergebnis" = "negative",                   
+                                              "Lepus europaeus " = "Lepus europaeus",                       
+                                              "negativ" = "negative",
+                                              "Rhinolophus h. hipposideros" = "Rhinolophus hipposideros",           
+                                              "schlechte ident." = "negative",                     
+                                              "sus scrofa" = "Sus scrofa",                         
+                                              "unauswertbar" = "negative"))          
 
 ## year 2015------------------------------------------------------------
 # read data 2015
 data2015 <- read.xlsx("data/bloodmeals-2015.xlsx", 1,stringsAsFactors=FALSE)
 
 # collapse mosquito species information from columns to 1 column
-dfdf <- data2015 %>% gather(species, specimens, Cx..spec:Mansonia.richiardii, na.rm = T)
+data2015_2 <- data2015 %>% gather(species, specimens, Cx..spec:Mansonia.richiardii, na.rm = T)
 
-BBB1 <- c("Kocker: Nycticorax An 3+4 05.09.17 negativ",
+# identify samples with additional host information (e.g. multiple species)
+add_info_2015_1 <- c("Kocker: Nycticorax An 3+4 05.09.17 negativ",
           "Bos taurus",
           "Felis catus",
           "Bovine with Burkett",
           "Kocher =Ardea purpurea ",
           "Gallus gallus m. Kocher An 3+4 05.09.17 negativ",
           "Homo sapiens m. Kocher")
-BBB2 <- c("Gallus gallus m. Kocher",
+add_info_2015_2 <- c("Gallus gallus m. Kocher",
           "Ardea purpurea am 22.11.17 (10157AA-411)",
           "Homo sapiens m. Kocher",
           "Homo sapiens am 22.11.17 (10157AA-411)") 
-CCC <- subset(dfdf, Bemerkungen.1 %in% BBB1 | Bemerkungen.2 %in% BBB2)
+add_info_2015 <- subset(data2015_2, Bemerkungen.1 %in% add_info_2015_1 | Bemerkungen.2 %in% add_info_2015_2)
 
-CCC$Host. <- as.character(c("Bos taurus/Nycticorax nycticorax",
-                            "Bos taurus",
-                            "Felis catus",
-                            "Sus scrofa/Bovine",
-                            "Homo sapiens/Ardea purpurea",
-                            "Bos taurus/Gallus gallus",
-                            "Bos taurus/Ardea purpurea",
-                            "Homo sapiens",
-                            "Bos taurus/Homo sapiens",
-                            "Bat/Homo sapiens",
-                            "Homo sapiens", "Homo sapiens", "Homo sapiens"))
+# information to replace for the host of the samples with additional host information (e.g. multiple species)
+add_info_2015$Host. <- as.character(c("Bos taurus/Nycticorax nycticorax",
+                                      "Bos taurus",
+                                      "Felis catus",
+                                      "Sus scrofa/Bovine",
+                                      "Homo sapiens/Ardea purpurea",
+                                      "Bos taurus/Gallus gallus",
+                                      "Bos taurus/Ardea purpurea",
+                                      "Homo sapiens",
+                                      "Bos taurus/Homo sapiens",
+                                      "Bat/Homo sapiens",
+                                      "Homo sapiens", "Homo sapiens", "Homo sapiens"))
 
-data2015b <- data.frame(sample_id = as.character(dfdf$Sample.ID), 
-                        site = as.character(dfdf$Trapping.site),
-                        date = as.character(dfdf$Date.of.collection),
-                        mosquito_species = as.character(dfdf$species),
-                        host = as.character(dfdf$Host.),stringsAsFactors=FALSE)
+# built new data.frame with relevant information
+data2015_3 <- data.frame(sample_id = as.character(data2015_2$Sample.ID), 
+                        site = as.character(data2015_2$Trapping.site),
+                        date = as.character(data2015_2$Date.of.collection),
+                        mosquito_species = as.character(data2015_2$species),
+                        host = as.character(data2015_2$Host.),stringsAsFactors=FALSE)
 
-data2015b$host[c(match(CCC$Sample.ID,data2015b$sample_id))] <- CCC$Host.
+# identify samples by sample id and replace host species names 
+data2015_3$host[c(match(CCC$Sample.ID,data2015_3$sample_id))] <- add_info_2015$Host.
 
 # homogenize names of host species
-data2015b$species_revalue <- revalue(data2015b$host, c(" Bos taurus" = "Bos taurus",           
-                                                       " Sus scrofa" = "Sus scrofa",              
-                                                       "Anas clypeata/Cygnus olor" = "Anatidae",
-                                                       "Anopheles sp." = "mosquito DNA",             
-                                                       "Bovine" = "Bovidae",                    
-                                                       "Canis lupus familiaris" = "Canis lupus",     
-                                                       "FCS contam. Bos taurus" = "FCS contanimation",   
-                                                       "fehlt" = "missing",                   
-                                                       "Hipposideros (Bat)" = "Chiroptera",       
-                                                       "kein ergebnis" = "negative",   
-                                                       "negativ" = "negative"))          
+data2015_3$host <- revalue(data2015_3$host, c(" Bos taurus" = "Bos taurus",           
+                                              " Sus scrofa" = "Sus scrofa",              
+                                              "Anas clypeata/Cygnus olor" = "Anatidae",
+                                              "Anopheles sp." = "mosquito DNA",             
+                                              "Bovine" = "Bovidae",                    
+                                              "Canis lupus familiaris" = "Canis lupus",     
+                                              "FCS contam. Bos taurus" = "FCS contanimation",   
+                                              "fehlt" = "missing",                   
+                                              "Hipposideros (Bat)" = "Chiroptera",       
+                                              "kein ergebnis" = "negative",   
+                                              "negativ" = "negative"))          
 
 ## year 2016------------------------------------------------------------
 # read data 2016
 data2016 <- read.xlsx("data/bloodmeals-2016.xlsx", 1)
-data2016 <- data2016[1:497,]
 
-eee <- str_split_fixed((data2016$Date.of.collection), "/", 2)[,2]
-eee2 <- ifelse(eee == "", NA, eee)
+# remove NAs
+data2016_2 <- data2016[1:497,]
 
+# extract date information of sampling
+#date_info_2016_1 <- str_split_fixed((data2016_2$Date.of.collection), "/", 2)[,2]
+#date_info_2016_2 <- ifelse(date_info_2016_1 == "", NA, date_info_2016_1)
 
-data2016b <- data.frame(sample_id = as.character(data2016$Zusatz.bezeichnung), 
-                        site = as.character(fillNAgaps(data2016$Trapping.site)),
-                        date = as.character(fillNAgaps(eee2)),
-                        mosquito_species = as.character(fillNAgaps(data2016$Mosquitoe.species)),
-                        host = as.character(data2016$bewertet.als))
+# built new data.frame with relevant information
+data2016_3 <- data.frame(sample_id = as.character(data2016_2$Zusatz.bezeichnung), 
+                        site = as.character(fillNAgaps(data2016_2$Trapping.site)),
+                        date = as.character(fillNAgaps(data2016_2$date_new)),
+                        mosquito_species = as.character(fillNAgaps(data2016_2$Mosquitoe.species)),
+                        host = as.character(data2016_2$bewertet.als))
 
-data2016b$species_revalue <- revalue(data2016b$host, c("Anopheles sp." = "mosquito DNA",             
-                                                       "Canis lupus familiaris" = "Canis lupus",     
-                                                       "FCS contam. Bos taurus" = "FCS contanimation",   
-                                                       "Pelophylax (Rana) ridibundus" = "Pelophylax ridibundus",                   
-                                                       "Mustela nivalis " = "Mustela nivalis"))
+# homogenize names of host species
+data2016_3$host <- revalue(data2016_3$host, c("Anopheles sp." = "mosquito DNA",             
+                                            "Canis lupus familiaris" = "Canis lupus",     
+                                            "FCS contam. Bos taurus" = "FCS contanimation",   
+                                            "Pelophylax (Rana) ridibundus" = "Pelophylax ridibundus",                   
+                                            "Mustela nivalis " = "Mustela nivalis"))
 
-ee <- rbind(data2014b, data2015b, data2016b)
+# rbind data from the 3 years
+data_all_years <- rbind(data2014_3, data2015_3, data2016_3)
 
-# remove 
-ee2 <- subset(ee, !(species_revalue %in% c("mosquito DNA",
-                                          "negative",
-                                          "missing",
-                                          "FCS contanimation",
-                                          "FCS contamination",
-                                          "confusing results")))
 
 # homogenize names of mosquitoes
-ee2$mosquito_species_new <- revalue(ee2$mosquito_species, c("Ae. vexans" = "Aedes vexans",
+data_all_years$mosquito_species <- revalue(data_all_years$mosquito_species, c("Ae. vexans" = "Aedes vexans",
                                                             "Ae./Oc. spec" = "Aedes spp.",                
                                                             "Ae./Oc. spec " = "Aedes spp.",
                                                             "Ae./Oc. spec." = "Aedes spp.",                 
@@ -181,7 +184,7 @@ ee2$mosquito_species_new <- revalue(ee2$mosquito_species, c("Ae. vexans" = "Aede
                                                             "An. maculipennis" = "Anopheles maculipennis s.l.",               
                                                             "An. maculipennis s.l." = "Anopheles maculipennis s.l.",  
                                                             "An. maculipennis s.l. ???" = "Anopheles maculipennis s.l.",      
-                                                            "An. plumbeus" = "Anopheles plumbeus", 
+                                                            "An. plumbeus" = "Anopheles algeriensis", 
                                                             "Anopheles claviger" = "Anopheles algeriensis",               
                                                             "Anopheles hyrcanus" = "Anopheles hyrcanus",   
                                                             "Anopheles maculipennis" = "Anopheles maculipennis s.l.",        
@@ -196,6 +199,7 @@ ee2$mosquito_species_new <- revalue(ee2$mosquito_species, c("Ae. vexans" = "Aede
                                                             "Culex pipiens/torrentium" ="Culex pipiens s.l./torrentium",     
                                                             "Culex.pipiens" ="Culex pipiens s.l./torrentium",                      
                                                             "culicoides (8)" = "Culicoides",
+                                                            "Culex.modestus" = "Culex modestus",
                                                             "Cx. modestus" = "Culex modestus",                  
                                                             "Cx. pip/tor" ="Culex pipiens s.l./torrentium",     
                                                             "Cx. pipiens/modestus/torrentiu" ="Culex pipiens s.l./torrentium",     
@@ -214,16 +218,8 @@ ee2$mosquito_species_new <- revalue(ee2$mosquito_species, c("Ae. vexans" = "Aede
                                                             "unidentified" = "unidentified",                  
                                                             "Ur. unguiculata" = "Uranotaenia unguiculata"))
 
-# resolve the 
-sort(unique(ee2$species_revalue))
-gdgdg <- str_split_fixed(ee2$species_revalue, "/", 2)
-ee2$species_revalue[which(gdgdg[,2] > 0)] <- gdgdg[which(gdgdg[,2] > 0),1]
-LLL <- ee2[which(gdgdg[,2] > 0),]
-LLL$species_revalue <- gdgdg[which(gdgdg[,2] > 0),2]
-ee3 <-rbind(ee2, LLL)
-
 # homogenize names of sites
-ee3$site <- revalue(ee3$site, c("Dunarea veche" = "Dunarea Veche",
+data_all_years$site <- revalue(data_all_years$site, c("Dunarea veche" = "Dunarea Veche",
                                 "Letea" = "Letea",
                                 "Sulina" = "Sulina",
                                 "L. Rosulet" = "Lake Rosulet",
@@ -239,8 +235,47 @@ ee3$site <- revalue(ee3$site, c("Dunarea veche" = "Dunarea Veche",
                                 "DV3" = "Dunarea Veche",
                                 "SL" = "Sulina"))
 
+# date
+dates_new <- do.call(rbind, strsplit(data_all_years$date, '[.]'))
+dates_new_2 <- paste(dates_new[,1], dates_new[,2], dates_new[,3])
+dates_new_3 <- as.POSIXct(dates_new_2, format = "%d%m%Y")
+
+data_all_years$date <- dates_new_3
+
+# resolve the 
+splitted_host_info <- str_split_fixed(data_all_years$host, "/", 2)
+data_all_years$host[which(splitted_host_info[,2] > 0)] <- splitted_host_info[which(splitted_host_info[,2] > 0),1]
+data_all_years_sub <- data_all_years[which(splitted_host_info[,2] > 0),]
+data_all_years_sub$host <- splitted_host_info[which(splitted_host_info[,2] > 0),2]
+data_all_years_2 <-rbind(data_all_years, data_all_years_sub)
+
+data_all_years_2$host <- revalue(data_all_years_2$host, c("Bat" = "Chiroptera",
+                                                          "Anas phathyrhyncos" = "Anas platyrhynchos",
+                                                          "Bovine" = "Bovidae",
+                                                          "FCS contanimation" = "FCS contamination"))
+
+
+# delete hosts
+ee2 <- subset(ee, !(species_revalue %in% c("confusing results"       
+                                           "missing"
+                                           "mosquito DNA"             
+                                           "negative" 
+                                           "FCS contanimation")))
+
+# delete taxa
+"Culicoides",
+"NA",
+"Simulidae",
+"unidentified" 
+
+
+# delete sites
 ee4 <- subset(ee3, !(ee3$site %in% c("GS", "CMZ")))
         
+
+
+
+
 
 et <- ddply(na.omit(ee4), .(mosquito_species_new, site),
       summarize,
@@ -248,7 +283,3 @@ et <- ddply(na.omit(ee4), .(mosquito_species_new, site),
       n_specimens = length(species_revalue),
       length(unique(species_revalue))/length(species_revalue))
 sum(et[,4])
-
-
-
-table((ee2$species_revalue))
